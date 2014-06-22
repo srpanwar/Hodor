061a03be5548f1a743e51682f7f07af59d03cf7d
srpanwar <panwarsr@yahoo.com>
2014-06-21 21:28:21 -0700

    commit

---
+++ Hodor/HDRSetupViewController.h
@@ -0,0 +1,13 @@
+//
+//  HDRSetupViewController.h
+//  Hodor
+//
+//  Created by Shailesh Panwar on 6/21/14.
+//  Copyright (c) 2014 Troupe Of Vagrants. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HDRSetupViewController : UIViewController
+
+@end
