e02bb7f7c3a97b7c1d4cf1c992d15c5ec1badaf5
srpanwar <panwarsr@yahoo.com>
2014-06-22 22:26:05 -0700

    commit

--- Hodor/HDRInviteTableViewCell.m
+++ Hodor/HDRInviteTableViewCell.m
@@ -22,10 +22,43 @@
 
 - (void) doContactSelection
 {
+    ABAddressBookRef addressBook = ABAddressBookCreateWithOptions(NULL, NULL);
+    
+    NSArray *allContacts = (__bridge NSArray *)ABAddressBookCopyArrayOfAllPeople(addressBook);
+    
+    for (int i =0; i < allContacts.count; i++)
+    {
+        ABRecordRef person = (__bridge ABRecordRef)([allContacts objectAtIndex:i]);
+        
+        if (person != nil)
+        {
+            ABMultiValueRef phones = ABRecordCopyValue(person, kABPersonPhoneProperty);
+            
+            if (ABMultiValueGetCount(phones) == 0)
+            {
+                CFErrorRef error = nil;
+                ABAddressBookRemoveRecord(addressBook, person, &error);
+                NSLog(@"Removing %@",(__bridge NSString *)ABRecordCopyCompositeName(person));
+            }
+            
+            CFRelease(phones);
+        }
+    }
+    
+    CFErrorRef saveError = nil;
+    ABAddressBookSave(addressBook, &saveError);
+    
     ABPeoplePickerNavigationController *picker = [[ABPeoplePickerNavigationController alloc] init];
     picker.peoplePickerDelegate = self;
+    picker.addressBook = addressBook;
     
+    NSArray *displayedItems = [NSArray arrayWithObjects:[NSNumber numberWithInt:kABPersonPhoneProperty], nil];
+    picker.displayedProperties = displayedItems;
+    
+    // Show the picker
     [self.viewController presentViewController:picker animated:YES completion:nil];
+    
+    CFRelease(addressBook);
 }
 
 - (void)peoplePickerNavigationControllerDidCancel:(ABPeoplePickerNavigationController *)peoplePicker
@@ -35,6 +68,12 @@
 
 - (BOOL)peoplePickerNavigationController:(ABPeoplePickerNavigationController *)peoplePicker
       shouldContinueAfterSelectingPerson:(ABRecordRef)person
+{
+    return YES;
+}
+
+- (BOOL)peoplePickerNavigationController:
+(ABPeoplePickerNavigationController *)peoplePicker shouldContinueAfterSelectingPerson:(ABRecordRef)person property:(ABPropertyID)property identifier:(ABMultiValueIdentifier)identifier
 {
     NSString* mobile = nil;
     ABMultiValueRef phones =(__bridge ABMultiValueRef)((__bridge NSString*)ABRecordCopyValue(person, kABPersonPhoneProperty));
@@ -46,21 +85,19 @@
     }
     NSLog(@"%@", mobile);
     
-    [peoplePicker dismissViewControllerAnimated:YES completion:^{
-        MFMessageComposeViewController *picker = [[MFMessageComposeViewController alloc] init];
-        picker.messageComposeDelegate = self;
-        picker.recipients = @[mobile];
-        
-        picker.body = [NSString stringWithFormat:@"I wanna HODOR you! Add my HODOR username %@ (if you don't have HODOR app get it here http://app)", @"HELLO"];
-        
-        [self.viewController presentViewController:picker animated:YES completion:nil];
-    }];
-    return NO;
-}
-
-- (BOOL)peoplePickerNavigationController:
-(ABPeoplePickerNavigationController *)peoplePicker shouldContinueAfterSelectingPerson:(ABRecordRef)person property:(ABPropertyID)property identifier:(ABMultiValueIdentifier)identifier
-{
+    if (mobile && mobile.length)
+    {
+        [peoplePicker dismissViewControllerAnimated:YES completion:^{
+            MFMessageComposeViewController *picker = [[MFMessageComposeViewController alloc] init];
+            picker.messageComposeDelegate = self;
+            picker.recipients = @[mobile];
+            
+            picker.body = [NSString stringWithFormat:@"I wanna HODOR you! Add my HODOR username %@ (if you don't have HODOR app get it here http://app)", @"HELLO"];
+            
+            [self.viewController presentViewController:picker animated:YES completion:nil];
+        }];
+    }
+    
     return NO;
 }
 
