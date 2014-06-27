8824d5d97641bf8b356a95e07b884eb4061b4fe1
srpanwar <panwarsr@yahoo.com>
2014-06-27 09:53:43 -0700

    commit

--- Hodor/HDRHomeTableViewCell.m
+++ Hodor/HDRHomeTableViewCell.m
@@ -41,7 +41,7 @@
             self.nameLabel.text = @"HODORED!";
             [self.busyIndicator stopAnimating];
             
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.5 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
                 self.nameLabel.text = name;
                 [self.busyIndicator stopAnimating];
                 [[HDRFriends instance] moveToTop:self.user];
