0440feac75dae7db84b71b14bfcd160e50124a8e
srpanwar <panwarsr@yahoo.com>
2014-06-27 01:22:52 -0700

    commit

--- Hodor/HDRAppDelegate.m
+++ Hodor/HDRAppDelegate.m
@@ -58,6 +58,11 @@
     [HDRUtils playSound];
 }
 
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult result))completionHandler
+{
+    [self application:application didReceiveRemoteNotification:userInfo];
+}
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
