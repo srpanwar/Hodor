e4bc2af2949a877fb1ede0636902ed0eb8be535d
srpanwar <panwarsr@yahoo.com>
2014-08-19 13:32:12 -0700

    comiit

--- Hodor/HDRListBox2.m
+++ Hodor/HDRListBox2.m
@@ -74,7 +74,7 @@
     if (msg.content.length)
     {
         UITextView *textView = [[UITextView alloc] initWithFrame:CGRectMake(0, 0, 290.0f, MAXFLOAT)];
-        textView.font = [UIFont fontWithName:@"Helvetica-Light" size:15.0f];
+        textView.font = [UIFont fontWithName:@"HelveticaNeue" size:15.0f];
         textView.text = msg.content;
         CGSize nSize = [textView sizeThatFits:CGSizeMake(290.0f, MAXFLOAT)];
         height = 7.0f + 24.0f + nSize.height;
